b9792e7bcc80522dea1b6687947dcef3e58b1b16
justinroche <60978112+justinroche@users.noreply.github.com>
2026-07-19 18:34:54 -0500

    Increase default duration

--- src/composables/useGameSettings.js
+++ src/composables/useGameSettings.js
@@ -2,5 +2,5 @@ import { ref } from 'vue';
 
 export const mode = ref('timer');
 export const requiredFingers = ref(4);
-export const durationSec = ref(60);
+export const durationSec = ref(120);
 export const countdownEnabled = ref(true);
